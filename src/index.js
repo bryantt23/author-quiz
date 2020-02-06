1785d058a12365ad84e885707c10c28292813237
bryant tunbutr <bryantt23@users.noreply.github.com>
2020-02-06 01:25:04 -0800

    Add 4 books with an author

--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import AuthorQuiz from './AuthorQuiz';
 import * as serviceWorker from './serviceWorker';
+import {shuffle, sample} from 'underscore';
 
 const authors = [
     {
       name: 'Mark Twain',
       imageUrl: 'images/authors/marktwain.jpg',
       imageSource: 'Wikimedia Commons',
-      books: ['The Adventures of Huckleberry Finn', 'Life on the Mississippi']
+      books: ['The Adventures of Huckleberry Finn']
     },
     {
       name: 'Joseph Conrad',
@@ -45,12 +46,23 @@ const authors = [
     }
   ];
 
+  function getTurnData(authors){
+      const allBooks=authors.reduce(function(p,c,i){
+          return p.concat(c.books);
+      }, []);
+      const fourRandomBooks=shuffle(allBooks).slice(0, 4);
+      const answer=sample(fourRandomBooks);
+
+      return {
+          books: fourRandomBooks, 
+          author: authors.find((author)=>
+            author.books.some((title)=>
+                title===answer))
+      }
+  }
+
 const state={
-    turnData:{
-        author: authors[0],
-        books: authors[0].books
-    }
-    
+    turnData: getTurnData(authors)    
 }
 
 ReactDOM.render(<AuthorQuiz {...state}/>, document.getElementById('root'));
