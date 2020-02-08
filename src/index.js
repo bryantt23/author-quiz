07437817d68c3045107078b5c039a3aefc23a36b
bryant tunbutr <bryantt23@users.noreply.github.com>
2020-02-08 15:20:08 -0800

    Add router

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter, Route} from 'react-router'
 import './index.css';
 import AuthorQuiz from './AuthorQuiz';
 import * as serviceWorker from './serviceWorker';
@@ -72,9 +73,14 @@ function onAnswerSelected(answer){
     render();
 }
 
-function render(){
-    ReactDOM.render(<AuthorQuiz {...state} onAnswerSelected={onAnswerSelected}/>, document.getElementById('root'));
+function App(){
+  return <AuthorQuiz {...state} onAnswerSelected={onAnswerSelected}/>;
 }
+
+function render(){
+    ReactDOM.render(<App/>,document.getElementById('root'));
+}
+
 render();
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
