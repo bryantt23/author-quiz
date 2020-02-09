ce4b8ffa2cd196372291a9e2119be091851beb54
bryant tunbutr <bryantt23@users.noreply.github.com>
2020-02-08 16:11:18 -0800

    Add ability to add author, book list, image url

--- src/AuthorQuiz.js
+++ src/AuthorQuiz.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import logo from './logo.svg';
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import './App.css';
 import './bootstrap.min.css';
 import PropTypes from 'prop-types';
 
-function Hero(){
+function Hero() {
   return (
     <div className="row">
       <div className="jumbotron col-10 offset-1">
@@ -16,10 +16,10 @@ function Hero(){
   )
 }
 
-function Turn({author, books, highlight, onAnswerSelected}){
-  function highlightToBgColor(highlight){
-    const mapping ={
-      'none': '', 
+function Turn({ author, books, highlight, onAnswerSelected }) {
+  function highlightToBgColor(highlight) {
+    const mapping = {
+      'none': '',
       'correct': 'green',
       'wrong': 'red'
     };
@@ -27,17 +27,17 @@ function Turn({author, books, highlight, onAnswerSelected}){
   }
 
   return (
-    <div className="row turn" style={{backgroundColor: highlightToBgColor(highlight)}}>
+    <div className="row turn" style={{ backgroundColor: highlightToBgColor(highlight) }}>
       <div className="col-4 offset-1">
-        <img src={author.imageUrl} className="authorimage" alt="Author"/>
+        <img src={author.imageUrl} className="authorimage" alt="Author" />
       </div>
       <div className="col-6">
-        {books.map((title)=><Book title={title} key={title} onClick={onAnswerSelected}/>)}
+        {books.map((title) => <Book title={title} key={title} onClick={onAnswerSelected} />)}
       </div>
     </div>
   )
 }
-Turn.propTypes={
+Turn.propTypes = {
   author: PropTypes.shape({
     name: PropTypes.string.isRequired,
     imageUrl: PropTypes.string.isRequired,
@@ -49,15 +49,15 @@ Turn.propTypes={
   highlight: PropTypes.string.isRequired
 };
 
-function Book({title, onClick}){
+function Book({ title, onClick }) {
   return (
-    <div className="answer" onClick={()=>{onClick(title)}}>
+    <div className="answer" onClick={() => { onClick(title) }}>
       <h4>{title}</h4>
     </div>
   )
 }
 
-function Continue(){
+function Continue() {
   return (
     <div className="row">
       <div className="jumbotron col-10 offset-1">
@@ -66,7 +66,7 @@ function Continue(){
   )
 }
 
-function Footer(){
+function Footer() {
   return (
     <div id="footer" className="row">
       <div className="col-12">
@@ -78,14 +78,14 @@ function Footer(){
   )
 }
 
-function AuthorQuiz({turnData, highlight, onAnswerSelected}) {
+function AuthorQuiz({ turnData, highlight, onAnswerSelected }) {
   return (
     <div className="container-fluid">
-      <Hero/>
-      <Turn {...turnData} highlight={highlight} onAnswerSelected={onAnswerSelected}/>
-      <Continue/>
+      <Hero />
+      <Turn {...turnData} highlight={highlight} onAnswerSelected={onAnswerSelected} />
+      <Continue />
       <p><Link to="/add">Add an Author</Link></p>
-      <Footer/>
+      <Footer />
     </div>
   );
 }
